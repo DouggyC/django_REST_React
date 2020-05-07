95ce3a9939223099a577c04775b5b070c8bb6d67
Douglas <douglas@mediushealth.org>
2020-05-07 16:49:22 +1000

    with redux state pattern, connected BE leads state and outputed to screen

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,6 +4,9 @@ import { render } from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 
+import { Provider } from 'react-redux'
+import store from '../store'
+
 export class App extends Component {
   constructor(props) {
     super(props);
@@ -36,12 +39,14 @@ export class App extends Component {
       //     </li>
       //   ))}
       // </ul>
-      <>
-        <Header />
-        <div className="container">
-          <Dashboard />
-        </div>
-      </>
+      <Provider store={store}>
+        <>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </>
+      </Provider>
     );
   }
 }
--- frontend/src/components/leads/Leads.js
+++ frontend/src/components/leads/Leads.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 export class Leads extends Component {
+  componentDidMount() {
+    this.props.getLeads();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Leads</h1>
-      </div>
-    )
+      <>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Message</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
-export default Leads
+// const mapDispatchToProps = () => ({
+//     doGetLeads: getLeads()
+// })
+
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
